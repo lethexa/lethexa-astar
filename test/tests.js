767e558bcb06c5f1db227fa197f12f7135650af3
Tim Leerhoff <tleerhof@web.de>
2015-08-01 22:21:39 +0200

    Added test

--- test/tests.js
+++ test/tests.js
@@ -1,11 +1,26 @@
-var assert = require("assert");
+var assert = require('assert');
+var astar = require('../lib/astar');
 
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal(-1, [1, 2, 3].indexOf(5));
-            assert.equal(-1, [1, 2, 3].indexOf(0));
-        });
-    });
+
+describe('AStarRouting', function () {
+	describe('#calculateRoute()', function () {
+		it('should return a valid route through the mesh', function () {
+			var mesh = new astar.Net();
+			mesh.addNode(1, {});
+			mesh.addNode(2, {});
+			mesh.addNode(3, {});
+			mesh.connect(1, 2);
+
+			var routing = new astar.AStarRouting();
+
+			//routing.calculateRoute(); 
+
+			assert.equal(-1, [1, 2, 3].indexOf(5));
+			assert.equal(-1, [1, 2, 3].indexOf(0));
+		});
+	});
 });
+
+
+
 
