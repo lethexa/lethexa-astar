8b789b36ba8e6f835aae0a7e8fadbd12f92e6a42
Tim Leerhoff <tleerhof@web.de>
2015-08-05 18:03:16 +0200

    Removed unnecessary test and console.logs

--- test/tests.js
+++ test/tests.js
@@ -9,26 +9,6 @@ describe('AStarRouting', function () {
 			mesh.addNode(1, {position: [0,0,0]});
 			mesh.addNode(2, {position: [1,0,0]});
 			mesh.addNode(3, {position: [2,0,0]});
-			mesh.connect(1, 2);
-			mesh.connect(2, 3);
-
-			var routing = new astar.AStarRouting(1, 3, mesh);
-
-			var result = routing.calculateRoute(); 
-			console.log('++++', result);
-
-			assert.equal(-1, [1, 2, 3].indexOf(5));
-			assert.equal(-1, [1, 2, 3].indexOf(0));
-		});
-	});
-
-
-	describe('#calculateRoute()', function () {
-		it('should return a valid route through the mesh', function () {
-			var mesh = new astar.Net();
-			mesh.addNode(1, {position: [0,0,0]});
-			mesh.addNode(2, {position: [1,0,0]});
-			mesh.addNode(3, {position: [2,0,0]});
 			mesh.addNode(4, {position: [3,0,0]});
 			mesh.addNode(5, {position: [3,0,0]});
 			mesh.connect(1, 2);
